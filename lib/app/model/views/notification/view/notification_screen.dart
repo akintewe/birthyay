02d1b42a98127e0e9ef197a26f3c02ab7b3f4175
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-03-30 21:46:24 +0100

    new screen

--- lib/app/model/views/notification/view/notification_screen.dart
+++ lib/app/model/views/notification/view/notification_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -13,9 +14,35 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Notifications'),
-      ),
-    );
+        body: Column(
+      children: [
+        SizedBox(
+          height: 35.5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Color.fromRGBO(110, 37, 136, 1),
+                )),
+            Text(
+              'Notifications',
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+              ),
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.more_vert_rounded,
+                ))
+          ],
+        ),
+      ],
+    ));
   }
 }
